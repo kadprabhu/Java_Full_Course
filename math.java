6cf57bdbd30aa75fc27f217db4dbe3fa0371a7c0
kadprabhu <94008870+kadprabhu@users.noreply.github.com>
2024-11-22 20:35:40 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,10 @@
+
+public class math {
+    public static void main(String[] args){
+        double x=3.14;
+        double y= -10;
+        double z = Math.max(x,y);
+        System.out.println(z);
+        
+    }    
+}
